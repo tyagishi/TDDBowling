b8bc29fa8a19fe3767890f55690b3dd1295f47d0
Tomoaki Yagishita <tyagishi@gmail.com>
2020-11-08 11:32:41 +0900

    Initial Commit

---
+++ TDDBowling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//
+//  Created by : Tomoaki Yagishita on 2020/11/08
+//  © 2020  SmallDeskSoftware
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TDDBowling/TDDBowlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDBowlingApp.swift
+//
+//  Created by : Tomoaki Yagishita on 2020/11/08
+//  © 2020  SmallDeskSoftware
+//
+
+import SwiftUI
+
+@main
+struct TDDBowlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
